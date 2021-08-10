08e36bdd5a6558bdda8dd0e91f713030cff45e63
Anh-vu <anhuvdinh@gmail.com>
2021-08-09 19:43:34 -0500

    commit tree 31d51ea4e77e2d97d6bc767a2a6cd62ce0113916

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 // Code your solution in this file!
+let blockA;
+let blockB;
+let blockHQ;
+let distanceInBlocks;
+let distanceInFeet;
+let distanceInFeetConversion;
+let start;
+let destination;
+let fare;
+let msg;
+
+function distanceFromHqInBlocks(blockA, blockHQ){
+    blockHQ = 42;
+    return distanceInBlocks = Math.abs(blockA-blockHQ);
+}
+
+function distanceFromHqInFeet(blockA, blockHQ = 42){
+    distanceInBlocks = Math.abs(blockA-blockHQ);
+    return distanceInFeet = distanceInBlocks*264;
+}
+
+function distanceTravelledInFeet(blockA, blockB){
+    distanceInBlocks = Math.abs(blockA-blockB);
+    return distanceInFeetConversion = distanceInBlocks*264;
+}
+
+function calculatesFarePrice(start, destination){
+    let x = Math.abs(destination-start)*264;
+
+    if(x <= 400){
+        fare = 0;
+        return fare;
+    }else if(x > 400 && x <= 2000){
+        fare = (x-400)*0.02;
+        return fare;
+    }else if (x > 2000 && x <= 2500){
+        fare = 25;
+        return fare;
+    }else if(x > 2500){
+        msg = "cannot travel that far";
+          return msg;
+    }
+}
